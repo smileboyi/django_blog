ae1281878e3b0008f7d64bbd8e30fda3fedeff05
smileboyi <15215212143@163.com>
2018-07-22 22:13:53 +0800

    分类和标签展示

--- blog/urls.py
+++ blog/urls.py
@@ -8,4 +8,6 @@ urlpatterns = [
 	# path(r'^$',views.index,name='blog_index'),
 	url(r'^$', views.index,name='blog_index'),
 	url(r'^(?P<blog_id>[0-9]+)',views.detail,name='blog_detail'),
+	url(r'^category/(?P<category_id>[0-9]+)/$',views.catagory,name='blog_category'),
+	url(r'^tag/(?P<tag_id>[0-9]+)/$',views.tag,name='blog_tag'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -142,3 +142,29 @@ def detail(request,blog_id):
 	entry.toc = md.toc
 
 	return render(request,'blog/detail.html',locals())
+
+
+
+def catagory(request,category_id):
+	c = models.Category.objects.get(id=category_id)
+	entries = models.Entry.objects.filter(category=c)
+
+	page = request.GET.get('page',1)
+	entry_list, paginator = make_paginator(entries, page)
+	page_data = pagination_data(paginator, page)
+
+	# 和index()用到的模板是一样的
+	return render(request, 'blog/index.html', locals())
+
+
+def tag(request,tag_id):
+	t = models.Tag.objects.get(id=tag_id)
+	if t.name == "全部":
+			entries = models.Entry.objects.all()
+	else:
+			entries = models.Entry.objects.filter(tags=t)
+	page = request.GET.get('page', 1)
+	entry_list, paginator = make_paginator(entries, page)
+	page_data = pagination_data(paginator, page)
+
+	return render(request, 'blog/index.html', locals())
